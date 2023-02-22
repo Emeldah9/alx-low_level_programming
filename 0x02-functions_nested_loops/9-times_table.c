968e98cf41684d60bb5d0c0531daa178cc995472
Emeldah9 <emeldahmakwana@gmail.com>
2023-02-21 21:46:40 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,9 @@
-* add extra space past single digit
+#include "main.h"
+/**
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
  */
 void times_table(void)
 {
@@ -32,3 +37,4 @@ int r, c, d;
 	}
 
 }
+
